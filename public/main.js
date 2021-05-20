d52bb79c7cff23364cc3e1727bf696e48259cbbd
Bianca Brown <biancakbrown58@gmail.com>
2021-05-20 11:33:30 -0400

    Showing player one what option they chose

--- public/main.js
+++ public/main.js
@@ -1,7 +1,31 @@
+let playerChoice = ''
+
+function handleClick(event) {
+  const buttonClicked = event.target.value
+  const choseRock = document.querySelector('.choice1')
+  choseRock.textContent = buttonClicked
+}
+function handleClickPaper(event) {
+  const buttonClickedP = event.target.value
+  const chosePaper = document.querySelector('.choice1')
+  chosePaper.textContent = buttonClickedP
+}
+
+function handleClickScissors(event) {
+  const buttonClickedS = event.target.value
+  const choseScissors = document.querySelector('.choice1')
+  choseScissors.textContent = buttonClickedS
+}
+
 function main() {
-  if (document.querySelector('h1.hello-world')) {
-    document.querySelector('h1.hello-world').textContent = 'Hello, World!'
-  }
+  const choiceR = document.querySelector('.player1 button.rock')
+  choiceR.addEventListener('click', handleClick)
+
+  const choiceP = document.querySelector('.player1 button.paper')
+  choiceP.addEventListener('click', handleClickPaper)
+
+  const choiceS = document.querySelector('.player1 button.scissors')
+  choiceS.addEventListener('click', handleClickScissors)
 }
 
 document.addEventListener('DOMContentLoaded', main)
